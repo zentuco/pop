5cf1bd8895b6ef4a2164ee522df8c553c93b34bb
Hugo Collado <hugo.collado91@gmail.com>
2018-08-08 19:53:21 +0800

    designs controller generated

---
+++ test/controllers/designs_controller_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class DesignsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get designs_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get designs_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get designs_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get designs_create_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get designs_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get designs_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get designs_destroy_url
+    assert_response :success
+  end
+
+end
