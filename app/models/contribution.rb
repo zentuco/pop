d9a4d10a8b02ba751709a537d18243e585edd892
Reed Van Anrooy <reed.vananrooy@gmail.com>
2018-08-09 14:36:31 +0800

    every model validated except for requests. will do after pull master

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,4 +1,5 @@
 class Contribution < ApplicationRecord
   belongs_to :user
   belongs_to :request
+  validates :tokens, presence: true
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,6 +1,7 @@
 class Request < ApplicationRecord
   belongs_to :user
   belongs_to :design
+  has_one :project
 
   enum kind: [:original, :improve]
 end
