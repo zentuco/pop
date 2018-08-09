e821d9b0df681ba152ee596ba71472f01830b835
Reed Van Anrooy <reed.vananrooy@gmail.com>
2018-08-09 18:58:00 +0800

    contribution form completed

---
+++ app/controllers/contributions_controller.rb
@@ -0,0 +1,21 @@
+class ContributionsController < ApplicationController
+
+  def create
+    @request = Request.find(params[:contribution][:request].to_i)
+    @contribution = Contribution.new(contribution_params)
+    @contribution.request = @request
+    @contribution.user = current_user
+    byebug
+    if @contribution.save!
+      redirect_to dashboard_path
+    else
+      redirect_to design_path(@request.design)
+    end
+  end
+
+  private
+
+  def contribution_params
+    params.require(:contribution).permit(:tokens)
+  end
+end
