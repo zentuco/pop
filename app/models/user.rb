a006a25e3dd0b360503c19661805ac4315d7b3be
Reed Van Anrooy <reed.vananrooy@gmail.com>
2018-08-10 17:01:07 +0800

    updated cards and dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   has_one :wallet
+  has_many :contributions
 
   def full_name
     first_name + " " + last_name
