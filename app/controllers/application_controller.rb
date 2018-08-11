355e42c29304167669dd3cffe4714b157ff3d5bf
Lance Purnell <lance.purnell@hotmail.fr>
2018-08-11 12:10:14 +0800

    fixed break when purchasing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,6 @@ class ApplicationController < ActionController::Base
   private
 
   def skip_pundit?
-    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/ || controller_name == "orders" || controller_name == "payments"
   end
 end
