2fbddf22b037ea3f7e8b7332749ff5dc9312ce4d
Reed Van Anrooy <reed.vananrooy@gmail.com>
2018-08-11 09:08:01 +0800

    intermediate changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def dashboard
     @orders = current_user.wallet.orders.where(state: 'paid')
-    @contributions = current_user.contributions
+    @contributions = current_user.contributions if current_user.contributions
     @projects = []
     current_user.contributions.each do |contribution|
       @projects << contribution.request.project if contribution.request.project
