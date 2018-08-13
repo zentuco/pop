39dd976e2e302179612a35fa0bcc8c5ae38c8858
Reed Van Anrooy <reed.vananrooy@gmail.com>
2018-08-13 12:37:24 +0800

    finally made hugo happy

--- app/models/design.rb
+++ app/models/design.rb
@@ -2,7 +2,7 @@ class Design < ApplicationRecord
   belongs_to :category
   has_many :attachments
   accepts_nested_attributes_for :attachments
-  accepts_nested_attributes_for :requests
+  # accepts_nested_attributes_for :requests
 
   has_many :requests
   validates :name, presence: true
