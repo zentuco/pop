ae5ff7cf3af5674474d8198bf494cee5e327c3b6
Reed Van Anrooy <reed.vananrooy@gmail.com>
2018-08-14 15:07:49 +0800

    can't input a contribution with negative tokens or greater than wallet amount

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,6 +1,6 @@
 class Contribution < ApplicationRecord
   belongs_to :user
   belongs_to :request
-  validates :tokens, presence: true
+  validates :tokens, presence: true, numericality: { greater_than_or_equal_to: 1 }
   validates :request, presence: true
 end
