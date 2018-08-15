1e38f514b4b1cd0f64ed8b8fcf6413d89a726238
James Douglas <jamesjimddouglas@hotmail.com>
2018-08-15 12:23:34 +0800

    Avatar links - see notes

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,6 @@ Rails.application.routes.draw do
   patch 'update_user_profile', to: 'users#update_user_profile', as: 'update_user_profile'
   delete 'destroy_user_profile', to: 'users#destroy', as: 'destroy_user_profile'
   get 'thingiverse', to: 'thingiverse#search_thingiverse'
+  get 'users/:id/profile', to: 'users#show', as: 'user_page'
 end
 
