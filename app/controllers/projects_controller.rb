f410d7ba3914d275dacd8f385642b8cd2e85739c
Reed Van Anrooy <reed.vananrooy@gmail.com>
2018-09-05 15:42:13 +0800

    projects index for designers is completed

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,6 +8,11 @@ class ProjectsController < ApplicationController
     authorize @project
   end
 
+  def index
+    @projects = policy_scope(Project).where(designer_id: params[:designer].to_i)
+    authorize @projects
+  end
+
   def create
     @request = Request.find(params[:request].to_i)
     @project = Project.new(accepted_price: @request.total_tokens)
@@ -15,7 +20,7 @@ class ProjectsController < ApplicationController
     @project.request = @request
     authorize @project
     if @project.save!
-      redirect_to dashboard_path
+      redirect_to @project
     else
       render 'designs#show'
     end
